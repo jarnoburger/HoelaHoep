c3ffb78e1506755a59887823ce933503ad08b5c4
jarnoburger <jarno@lichtjes.nl>
2020-08-04 22:04:05 +0200

    Een lap code om de leds te testen.

---
+++ LedTest/simple/simple.ino
@@ -0,0 +1,96 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// Released under the GPLv3 license to match the rest of the
+// Adafruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        5 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUM_LEDS 144 // Popular NeoPixel ring size
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 5 // Time (in milliseconds) to pause between pixels
+
+bool direction;
+int c;
+
+void setup() {
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+}
+
+void loop() {
+  //SnowSparkle(0, 0, 0, 10, random(1,10));
+int i;
+int m;
+
+
+  //for (i=0;i<255;i++)
+  //{
+  //setAll(i,i,i);
+  //showStrip();
+  //delay(10);
+  //} 
+
+  int speedDelay = 1;
+  int returnDelay = 50;
+  int red = 255;
+  int green = 255;
+  int blue = 255;
+  int eyeSize = 3;
+  LeftToRight(red, green, blue, eyeSize, speedDelay, returnDelay);
+  RightToLeft(red, green, blue, eyeSize, speedDelay, returnDelay);
+
+  //Sparkle(255, 255, 255, 63);
+
+  //meteorRain(0xff,0xff,0xff,6, 64, true, 10);
+
+  //Strobe(0xff, 0xff, 0xff, 10, 50, 1000);
+}
+
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue);
+  }
+  showStrip();
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H
+   // NeoPixel
+   pixels.setPixelColor(Pixel, pixels.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H
+   // NeoPixel
+   pixels.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
