bd3421f87c08980e68b44439321124b3711b5aae
Jarno Burger <jarno@lichtjes.nl>
2021-06-02 09:40:41 +0200

    C# project begonnen voor scannen frames

---
+++ LedHoepel V4 Mappert/Mappert/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mappert
+{
+    public partial class Form1 : Form
+    {
+        private readonly Pen _penCircles = new Pen(Color.IndianRed, 1);
+        private List<Wolkje> _wolkjes;
+        private readonly Random _random;
+        public Form1()
+        {
+            _random = new Random();
+            InitializeWolkjes();
+            InitializeComponent();
+
+            this.pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            for (var i = 0; i < _wolkjes.Count; i++)
+            {
+                var wolkje = _wolkjes[i];
+                var rect = new Rectangle(wolkje.Left, wolkje.Top, wolkje.Radius * 2, wolkje.Radius * 2);
+                graphics.DrawEllipse(_penCircles, rect);
+            }
+        }
+
+        private void InitializeWolkjes()
+        {
+            _wolkjes = new List<Wolkje>();
+            for (int i = 0; i < 40; i++)
+            {
+                var center = new Point(_random.Next(this.Width), _random.Next(this.Height));
+                var wolkje = new Wolkje(center,8);
+                _wolkjes.Add(wolkje);
+            }
+        }
+    }
+}
---
+++ LedHoepel V4 Mappert/Mappert/Wolkjes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mappert
+{
+    class Wolkje
+    {
+        private List<Point> _points;
+        private Point _center;
+        private int _radius;
+
+        public Wolkje(Point center, int radius)
+        {
+            _points = new List<Point>();
+            _center = center;
+            _radius = radius;
+        }
+
+        public Point Get(int index)
+        {
+            if (index < 0) throw new ArgumentException("Wolkjes has 11 pixels, Index can not be smaller then 0");
+            if (index >= 11) throw new ArgumentException("Wolkjes has 11 pixels, Index ca not be bigger then 10");
+            return _points[index];
+        }
+
+        public Point Center
+        {
+            get { return _center; }
+        }
+
+        public int Radius
+        {
+            get { return _radius; }
+        }
+
+        public int Left
+        {
+            get
+            {
+                return _center.X - _radius;
+            }
+        }
+
+        public int Right
+        {
+            get
+            {
+                return _center.X + _radius;
+            }
+        }
+
+        public int Top
+        {
+            get
+            {
+                return _center.Y - Radius;
+            }
+        }
+
+        public int Bottom
+        {
+            get
+            {
+                return _center.Y + Radius;
+            }
+        }
+    }
+}
